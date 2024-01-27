b903e85f91655df966209dea3092f9534674b7bf
Karkaranos <karkaranosyt@gmail.com>
2024-01-26 21:16:49 -0600

    Input p1

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class GameController : MonoBehaviour
 {
     public GameObject[] shelvesVis = new GameObject[9];
     Objects[,] shelvesCont = new Objects[3, 3];
     ObjectHandler oh;
+
+    private Player mouseControls;
+
+    private InputAction leftClick;
+    private InputAction rightClick;
 
     private void Start()
     {
@@ -16,15 +22,18 @@ public class GameController : MonoBehaviour
     }
     private void RefillAllShelves()
     {
+        int counter = 0;
         for(int i=0; i<3; i++)
         {
             for(int j=0; j<3; j++)
             {
                 shelvesCont[i, j] = oh.items[oh.WeighRandomNumber()];
-                shelvesVis[i+j].GetComponent<SpriteRenderer>().sprite = shelvesCont[i, j].visual;
+                shelvesVis[counter].GetComponent<SpriteRenderer>().sprite = shelvesCont[i, j].visual;
+                counter++;
             }
         }
 
     }
+
 
 }
